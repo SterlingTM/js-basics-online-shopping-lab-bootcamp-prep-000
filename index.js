5f29c24734da525e596afdf09a4c5e5409dbbf25
Sterling Moss <mtsilvertm@gmail.com>
2018-06-06 14:14:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,8 +24,28 @@ addToCart('Granny Smith');
 
 
 function viewCart() {
-  // write your code here
+  var cartItems = [];
+  if (cart.length === 0) {
+    console.log('Your shopping cart is empty.');
+
+  } else if (cart.length === 1) {
+    var printOneItem = `In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`;
+    console.log(printOneItem);
+
+  } else if (cart.length === 2) {
+    var printTwoItems = `In your cart, you have ${Object.keys(cart[cart.length-2])} at $${cart[cart.length-2][Object.keys(cart[cart.length-2])]} and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`;
+    console.log(printTwoItems);
+  
+    
+  } else {
+      for (var i = 0; i < cart.length-1; i++) {
+      cartItems.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`);
+          }
+          var printThreeOrMoreItems = `In your cart, you have${cartItems}, and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`;
+          console.log(printThreeOrMoreItems);
+  }
 }
+viewCart(cart);
 
 function total() {
   // write your code here
